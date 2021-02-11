61528d6906ec7852a0c62c5c45d943f85807fe52
Lara Landis <landiss77@gmail.com>
2021-02-10 20:02:22 -0500

    Add files via upload

---
+++ vieworders_v2.php
@@ -0,0 +1,46 @@
+<?php
+  // create short variable name
+  $document_root = $_SERVER['DOCUMENT_ROOT'];
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bob's Auto Parts - Customer Orders</title>
+  </head>
+  <body>
+    <h1>Bob's Auto Parts</h1>
+    <h2>Customer Orders</h2> 
+    <?php
+      @$fp = fopen("$document_root/class_projects/ch2/orders.txt", 'rb');
+      flock($fp, LOCK_SH); // lock file for reading
+
+      if (!$fp) {
+        echo "<p><strong>No orders pending.<br />
+              Please try again later.</strong></p>";
+        exit;
+      }
+	$outputstring = $date. "\t".$tireqty." tires \t".$oilqty." oil\t"
+                      .$sparkqty." spark plugs\t\$".$totalamount
+                      ."\t". $address."\n".$city."\t".$state."\t".$zip."\n".
+					  $email."\t".$phone."\n";
+
+		echo "<p>Order infromation is: ".htmlspecialchars($outputstring)."</p>";
+    
+	while (!feof($fp)) {
+         $order= fgets($fp);
+         echo htmlspecialchars($order)."<br />";
+    }
+
+      flock($fp, LOCK_UN); // release read lock
+
+      echo 'Final position of the file pointer is '.(ftell($fp));
+      echo '<br />';
+      rewind($fp);
+      echo 'After rewind, the position is '.(ftell($fp));
+      echo '<br />';
+
+      fclose($fp); 
+
+    ?>
+  </body>
+</html>
